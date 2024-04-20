f97b9b8fcda56b2b55b35b60f1b81deb60f99d79
“ruby” <rubyrajanadoor@gmail.com>
2024-04-19 20:08:03 -0400

    product layout

--- frontend/src/components/ProductList.js
+++ frontend/src/components/ProductList.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import { Star, StarFill } from 'react-bootstrap-icons';
 import { useUser } from './useUser';
 import { useCart } from './useCart';
@@ -32,39 +34,47 @@ export default function ProductList() {
     <div>
       <h1 className="mt-4">Product List</h1>
       <div>
-        {products?.map((product) => (
-          <Card className="my-4 ">
-            <div className="d-md-flex d-block product-card">
-              <img
-                style={{ width: 150 }}
-                alt={product?.title}
-                src={`http://localhost:4050/img/${product?.image}`}
-              />
-              <Card.Body>
-                <Card.Title>{product?.title}</Card.Title>
+        <Row>
+          {products?.map((product, i) => (
+            <Col key={i} xs={12} md={6} lg={4} className="my-2">
+              <div
+                className="my-4 text-center h-100"
+                style={{ border: '1px solid lightgray', borderRadius: 5 }}
+              >
                 <div>
-                  <StarFill />
-                  <StarFill />
-                  <StarFill />
-                  <StarFill />
-                  <Star />
+                  <img
+                    className="p-4"
+                    style={{ width: '100%' }}
+                    alt={product?.title}
+                    src={`http://localhost:4050/img/${product?.image}`}
+                  />
+                  <Card.Body>
+                    <Card.Title>{product?.title}</Card.Title>
+                    <div>
+                      <StarFill />
+                      <StarFill />
+                      <StarFill />
+                      <StarFill />
+                      <Star />
+                    </div>
+                    <div>
+                      <Card.Text>${product?.pricing}</Card.Text>
+                      {cart?.some((c) => c?.product?._id === product?._id) ? (
+                        <Button onClick={() => (window.location = '/cart')}>
+                          Go to Cart
+                        </Button>
+                      ) : (
+                        <Button onClick={() => addToCart(product)}>
+                          Add to Cart
+                        </Button>
+                      )}
+                    </div>
+                  </Card.Body>
                 </div>
-                <div>
-                  <Card.Text>${product?.pricing}</Card.Text>
-                  {cart?.some((c) => c?.product?._id === product?._id) ? (
-                    <Button onClick={() => (window.location = '/cart')}>
-                      Go to Cart
-                    </Button>
-                  ) : (
-                    <Button onClick={() => addToCart(product)}>
-                      Add to Cart
-                    </Button>
-                  )}
-                </div>
-              </Card.Body>
-            </div>
-          </Card>
-        ))}
+              </div>
+            </Col>
+          ))}
+        </Row>
       </div>
     </div>
   );
